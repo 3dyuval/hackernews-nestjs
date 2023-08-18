a2a389190d9a117ebf32736fce25ab7530619731
3dyuval <3dyuval@gmail.com>
2023-08-19 02:35:07 +0300

    added neondb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { ConfigModule } from '@nestjs/config';
-import { LinkModule } from './link/link.module';
-import { CommentModule } from './comment/comment.module';
-import { FeedModule } from './feed/feed.module';
-import * as path from 'path';
+import { ConfigModule } from '@nestjs/config'
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { LinkModule } from './link/link.module'
+import { CommentModule } from './comment/comment.module'
+import { FeedModule } from './feed/feed.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import * as path from 'path'
+import { GraphQLModule } from '@nestjs/graphql'
+
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env['DB_POSTGRES_HOST'],
-      port: Number.parseInt(process.env['DB_POSTGRES_PORT']),
-      database: process.env['DB_POSTGRES_CLIENT_DATABASE_NAME'],
-      username: process.env['DB_POSTGRES_USER'],
-      password: process.env['DB_POSTGRES_PASSWORD'],
+      url: `postgres://3dyuval:${process.env['DB_NEON_PASSWORD']}@ep-rapid-leaf-80187615.us-west-2.aws.neon.tech/hackernews?sslmode=require`,
+      type: 'postgres' as any,
       autoLoadEntities: true,
       synchronize: true,
       logging: true,
@@ -30,6 +27,7 @@ import * as path from 'path';
     LinkModule,
     CommentModule,
     FeedModule,
+    ConfigModule,
   ],
   controllers: [AppController],
   providers: [AppService],
