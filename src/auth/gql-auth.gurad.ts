ea77dbf622c0ba2b188accfd8023ce5cf16a8926
Yuval Dikerman <3dyuval@gmail.com>
2023-08-19 19:09:29 +0300

    cleanup

--- src/auth/gql-auth.gurad.ts
+++ src/auth/gql-auth.gurad.ts
@@ -1,28 +1,9 @@
 import { ExecutionContext, Injectable } from '@nestjs/common'
 import { GqlExecutionContext } from '@nestjs/graphql'
 import { AuthGuard } from '@nestjs/passport'
-import  * as Auth0Strategy  from 'passport-auth0'
-
-const strategy = new Auth0Strategy({
-  domain: process.env['AUTH0_DOMAIN'],
-  clientID: process.env['AUTH0_CLIENT_ID'],
-  clientSecret: process.env['AUTH0_CLIENT_SECRET'],
-  callbackURL: process.env['AUTH0_CALLBACK_URL'],
-},
-function(accessToken, refreshToken, extraParams, profile, done) {
-  /**
-   * Access tokens are used to authorize users to an API
-   * (resource server)
-   * accessToken is the token to call the Auth0 API
-   * or a secured third-party API
-   * extraParams.id_token has the JSON Web Token
-   * profile has all the information from the user
-   */
-  return done(null, profile);
-})
 
 @Injectable()
-export class GQLAuthGuard extends AuthGuard(strategy) {
+export class GQLAuthGuard extends AuthGuard('oidc') {
   constructor() {
     super()
   }
